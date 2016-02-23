7156aeebde0bf2ee760de85083b37fb68c763002
alexandre <alexandre@alexandre.com>
2016-02-23 12:04:53 -0300

    Material Alvorada

---
+++ src/polimorfismo/Main.java
@@ -0,0 +1,42 @@
+package polimorfismo;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Fish fi = new Fish();
+        CodFish cf = new CodFish();
+        
+        fi.colorFish();
+        cf.colorFish();
+        cf.prFish();
+        
+    }
+}
+
+class Fish{
+    
+    public void colorFish(){
+        
+        System.out.println("Red");
+        
+    }
+    
+}
+
+class CodFish extends Fish{
+    
+       public void colorFish(){
+         
+        System.out.println("Green");
+        
+    }
+      
+        public void prFish(){
+         
+            super.colorFish();
+            this.colorFish();
+            
+        }
+    
+}
